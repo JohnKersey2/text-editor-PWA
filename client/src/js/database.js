31f95161e10f63fba59f64ca013b0641fbd83588
John Kersey <johnkersey2@yahoo.com>
2023-07-12 18:48:33 -0400

    Added database functions

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,40 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Post to the database');
+
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email });
+
+  const result = await request;
+
+  console.log('🚀 - data saved to the database', result);
+  // console.error('putDb not implemented');
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async (id) => {
+  console.log('GET from the database');
+
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.get(id);
+
+  const result = await request;
+  console.log('result.value', result);
+  return result.value;
+};
+
+// console.error('getDb not implemented');
 
 initdb();
